b61bb0f8a3dc9d14f70e08128bc2a95fffeef3c1
sleeplesseditor <josh@bryson.me.uk>
2026-07-02 13:54:58 +0100

    Outputs added to JavaScript action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,8 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 
+// NOTE: Requires AWS S3 Bucket and secrets to be added to repo
+
 function run() {
     const bucket = core.getInput('bucket', { required: true });
     const bucketRegion = core.getInput('region', { required: true });
@@ -10,6 +12,9 @@ function run() {
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
     core.notice('Deploying to AWS S3 using JavaScript...');
+
+    const websiteUrl = `https://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
 }
 
 run();
