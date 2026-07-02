34629f78baa9f123fe4867a9fa190d029342dec0
sleeplesseditor <josh@bryson.me.uk>
2026-07-02 13:48:34 +0100

    Added custom deploy action - requires additional secrets to work

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,14 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
 import * as exec from '@actions/exec';
 
 function run() {
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
     core.notice('Deploying to AWS S3 using JavaScript...');
 }
 
